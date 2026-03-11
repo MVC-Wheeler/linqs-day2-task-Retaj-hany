73253c29eca748aedc9f42c3cfef71c73d7e0081
Retag Hany <Retag.Hany@ELSEWEDY-SCHOOL.LOCAL>
2026-03-11 13:30:35 +0200

    task2 retaj

---
+++ Day2EngMai/Day2EngMai/Exetention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2EngMai
+{
+    public static class Exetention
+    {
+        public static int CoubtWords(this string s)
+        {
+            int count = 1;
+            foreach (char c in s)
+            {
+                if(c ==  ' ') count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Day2EngMai/Day2EngMai/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day2EngMai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sb = "worled planet fish elephant go hard easy";
+            Console.WriteLine(sb.CoubtWords());
+        }
+    }
+}
